4c8667451578a4ae0f6300ef814d2d1bc9eeeca3
Fareolla <vdovichenko83@hotmail.com>
2019-02-01 21:55:55 +0200

    [Lesson4] Added
    Lesson practice.

---
+++ Lesson4/Step1/mylib.cpp
@@ -0,0 +1,41 @@
+#include "mylib.h"
+#include <cmath>
+#include <limits>
+
+namespace Mylib {
+
+double Exponentiation (double digit, double exponent)
+{
+    // При 0 степени результат будет 1
+    double result {1.0};
+
+    if (fuzzyCompare (exponent, 0.0))
+    {
+        return result;
+    }
+
+    double temp_exp {(exponent > 0) ? exponent : -exponent};
+
+    for (int i {0}; i< temp_exp; ++i)
+    {
+        result *= digit;
+    }
+    if (exponent < 0)
+    {
+        result = 1 / result;
+    }
+    return result;
+}
+
+    bool fuzzyCompare (double left, double right)
+    {
+        bool result {false};
+
+        double diff{std::fabs(left - right)};
+
+        result = diff < std::numeric_limits<double>::epsilon();
+
+        return result;
+    }
+
+} // namespace Mylib
---
+++ Lesson4/Step1/mylib.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace Mylib {
+
+double Exponentiation (double digit, double exponent);
+
+bool fuzzyCompare (double left, double right);
+
+} // namespace Mylib
+
